485b1bfb4848af3033da2c7fb14fd9b5b169c463
frankkruithof <46526195+frankkruithof@users.noreply.github.com>
2021-03-16 00:11:53 +0100

    Short and long feature (test)
    Added shorting and longing tests
    Replaced confirmation price check with vwap check

--- backend/server.js
+++ backend/server.js
@@ -28,58 +28,101 @@ app.get('/orders', async (req, res) => {
     })
 })
 
-let stake = 100
+const stake = 100
+const leverage = 1
 let alerts = {}
 let key, sellPrice, pnl
 app.post("/hook", (req, res, next) => {
     // Create ticker object if it doesnt exist
     key = req.body.ticker + req.body.interval
+    
     if(!(key in alerts)) {
         alerts[key] = {}
     }
-
+  
     // If bar close price 
     if(req.body.confirm == 'buy' || req.body.confirm == 'sell') {
-        if(req.body.confirm == 'buy' && global.ticker[req.body.ticker].close < alerts[key].price) {
-            Object.assign(alerts[key], {active: false, PnL: pnl})
-            console.info('Bar close price lower than order price, closing trade')
-            console.info(`Sold ${alerts[key].orderQ} ${key} @ ${req.body.price}, PnL = ${(sellPrice - alerts[key].total).toFixed(2)}`)
+        if(alerts[key].hasOwnProperty('trend')) {
+            if(req.body.confirm == 'buy' && req.body.vwap < 0) {
+                sellPrice = alerts[key].orderQ * req.body.price
+                if(alerts[key].hasOwnProperty('PnL')) {
+                    pnl = (alerts[key].PnL + (sellPrice - alerts[key].total)) - 0.08
+                } else {
+                    pnl = (sellPrice - alerts[key].total) - 0.08
+                }
+                Object.assign(alerts[key], {active: false, PnL: pnl})
+                console.info('Bar close price lower than order price, closing position')
+                console.info(`${req.body.time} - Closed long ${alerts[key].orderQ} ${key} @ ${req.body.price}, PnL = ${(sellPrice - alerts[key].total).toFixed(2)}`)
+            } else if(req.body.confirm == 'sell' && req.body.vwap > 0) {
+                sellPrice = alerts[key].orderQ * req.body.price
+                if(alerts[key].hasOwnProperty('PnL')) {
+                    pnl = (alerts[key].PnL + (alerts[key].total - sellPrice)) - 0.08
+                } else {
+                    pnl = (alerts[key].total - sellPrice) - 0.08
+                }
+                Object.assign(alerts[key], {active: false, PnL: pnl})
+                console.info('Bar close price higher than order price, closing position')
+                console.info(`${req.body.time} - Closed short ${alerts[key].orderQ} ${key} @ ${req.body.price}, PnL = ${(alerts[key].total - sellPrice).toFixed(2)}`)
+            }
         }
     } else {
-        Object.assign(alerts[key], {price: req.body.price, interval: req.body.interval, trend: req.body.trend})
-
-        if(alerts[key].trend == 'up') {
-            if(!alerts[key].active) {
-                Object.assign(alerts[key], {active: true, orderQ: stake / req.body.price, total: (stake / req.body.price) * req.body.price})
-                console.info(`Bought ${alerts[key].orderQ} ${key} @ ${req.body.price} (Total: ${alerts[key].total})`)
-            }
-        } else {
-            if(alerts[key].active) {
+        if(req.body.trend == 'up') {
+            if(alerts[key].trend == 'down' && alerts[key].active) {
                 sellPrice = alerts[key].orderQ * req.body.price
-                
                 if(alerts[key].hasOwnProperty('PnL')) {
-                    pnl = alerts[key].PnL + (sellPrice - alerts[key].total)
+                    pnl = alerts[key].PnL + (alerts[key].total - sellPrice) - 0.08
                 } else {
-                    pnl = sellPrice - alerts[key].total
+                    pnl = (alerts[key].total - sellPrice)  - 0.08
                 }
-
-                Object.assign(alerts[key], {active: false, PnL: pnl})
-                console.info(`Sold ${alerts[key].orderQ} ${key} @ ${req.body.price}, PnL = ${(sellPrice - alerts[key].total)}`)
-
+                console.info(`${req.body.time} - Closed short position (${leverage}x) ${alerts[key].orderQ} ${key} @ ${req.body.price}, PnL = ${(alerts[key].total - sellPrice)}`)
+                Object.assign(alerts[key], {PnL: pnl, active: true, trend: req.body.trend, orderQ: stake / req.body.price, total: (stake / req.body.price) * req.body.price, price: req.body.price, interval: req.body.interval})
+                console.info(`${req.body.time} - Opened long position (${leverage}x) ${alerts[key].orderQ} ${key} @ ${req.body.price}`)
+            } else if(!alerts[key].hasOwnProperty('trend')) {
+                Object.assign(alerts[key], {active: true, trend: req.body.trend, orderQ: stake / req.body.price, total: (stake / req.body.price) * req.body.price, price: req.body.price, interval: req.body.interval})
+                console.info(`${req.body.time} - Opened long position (${leverage}x) ${alerts[key].orderQ} ${key} @ ${req.body.price}`)
+            } else if(alerts[key].trend == 'up' && !alerts[key].active){
+                Object.assign(alerts[key], {active: true, trend: req.body.trend, orderQ: stake / req.body.price, total: (stake / req.body.price) * req.body.price, price: req.body.price, interval: req.body.interval})
+                console.info(`${req.body.time} - Opened long position (${leverage}x) ${alerts[key].orderQ} ${key} @ ${req.body.price}`)
+            }
+        } else if(req.body.trend == 'down') {
+            if(alerts[key].trend == 'up' && alerts[key].active) {
+                sellPrice = alerts[key].orderQ * req.body.price
+                if(alerts[key].hasOwnProperty('PnL')) {
+                    pnl = (alerts[key].PnL + (sellPrice - alerts[key].total)) - 0.08
+                } else {
+                    pnl = (sellPrice - alerts[key].total)  - 0.08
+                }
+                console.info(`${req.body.time} - Closed long position (${leverage}x) ${alerts[key].orderQ} ${key} @ ${req.body.price}, PnL = ${(sellPrice - alerts[key].total)}`)
+                Object.assign(alerts[key], {PnL: pnl, active: true, trend: req.body.trend, orderQ: stake / req.body.price, total: (stake / req.body.price) * req.body.price, price: req.body.price, interval: req.body.interval})
+                console.info(`${req.body.time} - Opened short position (${leverage}x) ${alerts[key].orderQ} ${key} @ ${req.body.price}`)
+            } else if(!alerts[key].hasOwnProperty('trend')) {
+                Object.assign(alerts[key], {active: true, trend: req.body.trend, orderQ: stake / req.body.price, total: (stake / req.body.price) * req.body.price, price: req.body.price, interval: req.body.interval})
+                console.info(`${req.body.time} - Opened short position (${leverage}x) ${alerts[key].orderQ} ${key} @ ${req.body.price}`)
+            } else {
+                Object.assign(alerts[key], {active: true, trend: req.body.trend, orderQ: stake / req.body.price, total: (stake / req.body.price) * req.body.price, price: req.body.price, interval: req.body.interval})
+                console.info(`${req.body.time} - Opened short position (${leverage}x) ${alerts[key].orderQ} ${key} @ ${req.body.price}`)
             }
         }
     }
-
+    if(req.body.confirm != 'buy' || req.body.confirm != 'sell') {
     console.info('Current PnL \n')
-    for(const [val] of Object.entries(alerts)) {
-        if(alerts[val].hasOwnProperty('PnL')) {
-            console.info(`${val} PnL = ${(alerts[val].PnL)}`)
-        } else {
-            console.info(`${val} PnL = 0 \n`)
+        for(const [val] of Object.entries(alerts)) {
+            if(alerts[val].hasOwnProperty('PnL')) {
+                console.info(`${val} PnL = ${(alerts[val].PnL)}`)
+            } else {
+                console.info(`${val} PnL = 0 \n`)
+            }
         }
     }
 
 })
+
+function confirmBuy(coin, price, interval) {
+    let timeout
+    setTimeout(function() {
+
+    }, )
+}
 
 app.get('/scalping', async (req, res) => {
 
